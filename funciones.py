fb8a6aaeb9f0960b7a331c5c324795de91610db2
Ricardo <ricardo@dgfoot.com>
2017-02-27 22:33:59 -0300

    add funciones.py

--- funciones.py
+++ funciones.py
@@ -9,6 +9,19 @@ def division(v_1,v_2):
 def mostrar_resultado(funcion):
 	print(funcion(4,4))
 
+def muchos_numeros(*args):##por convencion se debe llamar asi a esa variable
+	print(type(args))
+	print(args)
+	suma_total=0
+	for x in args:
+		suma_total +=x
+
+	return suma_total
+
+def muchos_parametros(**kwargs):
+	print(type(kwargs))
+	print(kwargs)
+
 print(sumar(3,2))
 print(sumar(valor_2=4, valor_1=1))
 print(sumar(valor_1=4))
@@ -31,5 +44,17 @@ nuevo_sumar = sumar
 print(nuevo_sumar(4,4))
 print("--")
 mostrar_resultado(sumar)
+print("--")
+total = muchos_numeros(2,3,4,5,6)
+print(total)
 
+print("--")
 
+solucion = muchos_parametros(x=5,y="ok",z=True, http=1.101)
+print(solucion)
+
+print("---")
+
+operacion = lambda x_1, x_2 : x_1 + x_2 ##una forma de hacer funciones
+
+print(operacion(1,1))
